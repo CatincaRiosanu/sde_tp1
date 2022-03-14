92674257514c7084af7721ade637573266b1879b
Catinca Elena Riosanu <catinca.riosanu@stud.fils.upb.ro>
2022-03-14 22:08:16 +0200

    test prim

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -11,16 +11,31 @@ fn ver_add() {
 }
 
 #[test]
-    fn ver_remove() {
-        let mut vector1 = Vector::new();
-        vector1.add(5);
-        vector1.add(3);
-        vector1.add(7);
+fn ver_remove() {
+    let mut vector1 = Vector::new();
+    vector1.add(5);
+    vector1.add(3);
+    vector1.add(7);
 
-        vector1.remove(5);
-       // vector1.remove(9); Cum verific daca vreau sa dau remove la o valoare care nu exista?
+    vector1.remove(5);
+    // vector1.remove(9); Cum verific daca vreau sa dau remove la o valoare care nu exista?
 
-       assert!(vector1.lista[1] == 7);  
+    assert!(vector1.lista[1] == 7);
+}
 
-    }
+#[test]
 
+fn ver_prim() {
+    let mut vector1 = Vector::new();
+
+    vector1.add(5);
+    vector1.add(4);
+    vector1.add(3);
+    vector1.add(7);
+    vector1.add(6);
+
+    let mut vector2 = vector1.prim();
+
+    assert!(vector2.lista[0] == 3);
+
+}
